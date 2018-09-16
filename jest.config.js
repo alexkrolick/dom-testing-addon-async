8374080c2067b85cb3eda466059134bc3ba0960b
Alex Krolick <alexander.krolick@gmail.com>
2018-09-16 15:22:15 -0700

    setup ci

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
-const jestConfig = require('kcd-scripts/jest')
+const jestConfig = require("kcd-scripts/jest");
 
 module.exports = Object.assign(jestConfig, {
-  testEnvironment: 'jest-environment-jsdom',
-})
+  testEnvironment: "jest-environment-jsdom",
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+      files: 100,
+    },
+  },
+});
